c695bb3c048eb7e9320892da390a3d9ca9b32e5d
Akriti Yadav <akritiyadav0214@gmail.com>
2026-02-28 23:52:47 +0600

    bubble sort

--- recursionwithstring.cpp
+++ recursionwithstring.cpp
@@ -17,24 +17,39 @@ using namespace std;
 
 //     return 0;
 // }
-bool checkpalindrome(string name,int i,int j){
-    if(i>j)
-    return true ;
-    if(name[i]!=name[j])
-    return false;
-    else{
-    return checkpalindrome(name,i+1,j-1);
+// bool checkpalindrome(string name,int i,int j){
+//     if(i>j)
+//     return true ;
+//     if(name[i]!=name[j])
+//     return false;
+//     else{
+//     return checkpalindrome(name,i+1,j-1);
+//     }
+// }
+// int main(){
+//     string name="abcde";
+//     cout<<endl;
+//     bool ispalindrome=checkpalindrome(name ,0,name.size()-1);
+//     if(ispalindrome){
+//         cout<<"string is palindrome";
+//     }
+//     else{
+//         cout<<"string is not palindrome";
+//     }
+//     return 0;
+// }
+// bubble sort
+void sortArray(int *arr,int n){
+    if(n==0||n==1){
+        return;
     }
+    for(int i=0;i<n;i++){
+        if(arr[i]>arr[i+1]){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+    sortArray(arr,n-1);
 }
 int main(){
-    string name="abcde";
-    cout<<endl;
-    bool ispalindrome=checkpalindrome(name ,0,name.size()-1);
-    if(ispalindrome){
-        cout<<"string is palindrome";
-    }
-    else{
-        cout<<"string is not palindrome";
-    }
-    return 0;
+
 }
