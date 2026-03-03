ec068ba6c46c2a4bda7639715e6adb4faf6bb58c
Akriti Yadav <akritiyadav0214@gmail.com>
2026-03-03 23:36:11 +0600

    update

---
+++ combinationsum.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+    private:
+    void sum(int ind,int target,vector<int>& arr,vector<vector<int>>& ans,vector<int>&ds){
+        if(ind==arr.size()){
+            if(target==0){
+                ans.push_back(ds);
+            }
+            return ;
+        }
+        if(arr[ind]<=target){
+            ds.push_back(arr[ind]);
+            sum(ind,target-arr[ind],arr,ans,ds);
+            ds.pop_back();
+        }
+        sum(ind+1,target,arr,ans,ds);
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+       vector<vector<int>>ans;
+       vector<int>ds;
+       sum(0,target,candidates,ans,ds);
+       return ans;
+    }
+};
+int main(){
+    Solution s;
+    vector<int>candidates={2,3,6,7};
+    int target=7;
+    vector<vector<int>>ans=s.combinationSum(candidates,target);
+    for(auto i:ans){
+        for(auto j:i){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
