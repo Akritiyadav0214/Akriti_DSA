d5ccde97bea34f4d4f7798c15d90aca82c5e7d0d
Akriti Yadav <akritiyadav0214@gmail.com>
2026-03-26 23:46:22 +0530

    update

---
+++ isomorphicstring.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.length() != t.length()) return false;
+
+    unordered_map<char, char> mapST;
+    unordered_map<char, char> mapTS;
+
+    for (int i = 0; i < s.length(); i++) {
+        char c1 = s[i];
+        char c2 = t[i];
+
+        // Check mapping from s to t
+        if (mapST.count(c1)) {
+            if (mapST[c1] != c2)
+                return false;
+        } else {
+            mapST[c1] = c2;
+        }
+
+        // Check mapping from t to s
+        if (mapTS.count(c2)) {
+            if (mapTS[c2] != c1)
+                return false;
+        } else {
+            mapTS[c2] = c1;
+        }
+    }
+
+    return true;
+} 
+    
+};
