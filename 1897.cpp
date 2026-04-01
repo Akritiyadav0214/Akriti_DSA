dc932c3da6daa481b64a8497700c534f522e195c
Akriti Yadav <akritiyadav0214@gmail.com>
2026-04-01 23:06:13 +0530

    update

---
+++ 1897.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        unordered_map<char, int> counts;
+        for (auto a  : words) {
+            for (char x : a) {
+                counts[x]++;
+            }
+        }
+        
+        int n = words.size();
+        for (auto a : counts) {
+            if (a.second % n != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
