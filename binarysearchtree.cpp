adf202a3b59c92415df53ce2904a67c49b831a69
Akriti Yadav <akritiyadav0214@gmail.com>
2026-04-07 22:30:54 +0530

    update

---
+++ binarysearchtree.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int d){
+        this->data=d;
+        this->left=NULL;
+        this->right=NULL;
+
+    }
+};
+
+Node* inserttobst(Node* root,int d){
+    if(root==NULL){
+        root=new Node(d);
+        return root;
+    }
+    if(d>root->data){
+        root->right=inserttobst(root->right,d);
+    }
+    else{
+        root->left=inserttobst(root->left,d);
+    }
+    return root;
+
+}
+
+void takeinput(Node* &root){
+    int data;
+    cin>>data;
+    while(data!=-1){
+        root=inserttobst(root,data);
+        cin>>data;
+    }
+}
+
+int main(){
+    Node* root=NULL;
+    cout<<"enter the data to create bst"<<endl;
+    takeinput(root);
+    return 0;
+}
