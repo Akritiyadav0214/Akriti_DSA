6f4fe2609a8e6019f51f0ac200e1aec19cc0ef89
Akriti Yadav <akritiyadav0214@gmail.com>
2026-04-09 22:35:28 +0530

    queue

---
+++ queue.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+    queue<int>q;
+    q.push(11);
+    q.push(15);
+    cout<<"size of queue"<<q.size()<<endl;
+    return 0;
+}
