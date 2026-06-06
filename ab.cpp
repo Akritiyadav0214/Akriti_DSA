5295e305b025534bcc8442445d512e442713a802
Akriti Yadav <akritiyadav0214@gmail.com>
2026-06-06 23:51:07 +0530

    update

---
+++ ab.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            totalSum -= nums[i]; // now totalSum becomes right sum
+
+            ans[i] = abs(leftSum - totalSum);
+
+            leftSum += nums[i];
+        }
+
+        return ans;
+    }
+};
