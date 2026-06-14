0a8015cf624605db05e8f0ea15d8d6f7964f4dd2
Akriti Yadav <akritiyadav0214@gmail.com>
2026-06-14 23:33:01 +0530

    update

---
+++ invertbinarytree.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+ struct TreeNode {
+      int val;
+     TreeNode *left;
+     TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+         TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==nullptr){
+            return nullptr;
+        }
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* current=q.front();
+            q.pop();
+            swap(current->left,current->right);
+            if(current->left)
+            q.push(current->left);
+            if(current->right)
+            q.push(current->right);
+        }
+        return root;
+
+    }
+};
