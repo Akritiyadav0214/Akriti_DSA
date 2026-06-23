f41590a59500fbe4420f58671de1263cda058728
Akriti Yadav <akritiyadav0214@gmail.com>
2026-06-23 20:33:56 +0530

    update

--- lee.cpp
+++ lee.cpp
@@ -1,14 +1,21 @@
 #include<iostream>
-
+#include<vector>
 using namespace std;
 class Solution {
 public:
-    double angleClock(int hour, int minutes) {
-        double minuteAngle = 6.0 * minutes;
-        double hourAngle = 30.0 * (hour % 12) + 0.5 * minutes;
-
-        double diff = abs(hourAngle - minuteAngle);
-
-        return min(diff, 360.0 - diff);
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty())
+            return "";
+            string ans="";
+            for(int i=0;i,i<strs[0].size();i++){
+                char ch=strs[0][i];
+                for(int j=1;j<strs.size();j++){
+                    if(i>=strs[j].size()||strs[j][i]!=ch)
+                    return ans;
+                }
+                ans+=ch;
+            }
+        
+        return ans;
     }
 };
